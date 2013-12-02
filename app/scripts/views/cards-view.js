f77ca0b66565257c403e639c9d7dd387d76b4a9e
Jeremy Kahn <jeremyckahn@gmail.com>
2013-12-01 19:51:31 -0600

    Positions cards in 3D space and colors them.

--- app/scripts/views/cards-view.js
+++ app/scripts/views/cards-view.js
@@ -21,7 +21,7 @@ define([
   var ROWS = 2;
   var COLUMNS = 2;
   var LAYERS = 4;
-  var ZOOM_PIXELS = 100;
+  var LAYER_DEPTH = 100;
 
   var CardsView = Backbone.View.extend({
     template: JST['app/scripts/templates/cards-tmpl.ejs']
@@ -54,6 +54,14 @@ define([
       if (this._sampledCardWidth === null) {
         this.measureAndStoreCardDimensions($card);
       }
+
+      var transformX = (x * this._sampledCardWidth) + 'px';
+      var transformY = (y * this._sampledCardHeight) + 'px';
+      var transformZ = (z * -LAYER_DEPTH) + 'px';
+      $card.css('transform',
+          'translate3d(' + transformX + ', ' + transformY + ', ' + transformZ +
+          ')');
+      $card.attr('data-layer', z);
 
       this._card$els[z][y][x] = $card;
       this.$el.append($card);
